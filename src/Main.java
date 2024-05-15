22a39ccb08dea5ffdad1547d72caa26f22fb7516
Maarten Postma <maarten.postma1000@gmail.com>
2024-05-15 15:33:49 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+  public static void main(String[] args) {
+    int number;
+    int input1;
+    int input2;
+    String name;
+
+  }
+  public static void hello() {
+    System.out.println("Hello");
+  }
+
+  public static void positiveOrNegative(int number) {
+
+    if (number > 0) {
+      System.out.println("This number is positive!");
+    } else {
+      System.out.println("This number is - negative!");
+    }
+  }
+
+
+  public static void postiveOrZeroOrNegativ(int number) {
+    if (number > 0) {
+      System.out.println("This number is positive!");
+    } else if (number == 0) {
+      System.out.println("This number is zero");
+    }
+    else {
+      System.out.println("This number is negative!");
+    }
+  }
+
+  public static void bartender() {}
+
+}
